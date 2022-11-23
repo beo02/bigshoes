3e569708d4f817726d74e3729dd51a3a8336615b
beo02 <102813073+beo02@users.noreply.github.com>
2022-11-23 19:26:51 +0700

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,15 +1,4 @@
 <?php
-        $host = "mysql5030"; //địa chỉ mysql server sẽ kết nối đến
-        $dbname="db_a9046f_bigshoe"; //tên database sẽ kết nối đến
-        $username = "a9046f_bigshoe"; //username để kết nối đến database 
-        $password = "thanh1812"; // mật khẩu để kết nối đến database
-        $conn = mysqli_connect($servername, $username, $password, $database);
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
-        echo “Connected successfully”;
-        mysqli_close($conn);
-
     function pdo_get_connection(){
         $pdo = new PDO("mysql:host=mysql5030;dbname=db_a9046f_bigshoe",'a9046f_bigshoe','thanh1812');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
