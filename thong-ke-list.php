8548bd1ff10227206e7d4e39276d06e7077f5170
beo02 <102813073+beo02@users.noreply.github.com>
2022-11-23 23:14:24 +0700

    Update thong-ke-list.php

--- thong-ke-list.php
+++ thong-ke-list.php
@@ -180,13 +180,13 @@
      <!-- /. WRAPPER  -->
     <!-- JS Scripts-->
     <!-- jQuery Js -->
-    <script src="../../../css/admin/js/jquery-1.10.2.js"></script>
+    <script src="../css/admin/js/jquery-1.10.2.js"></script>
       <!-- Bootstrap Js -->
-    <script src="../../../css/admin/js/bootstrap.min.js"></script>
+    <script src="../css/admin/js/bootstrap.min.js"></script>
     <!-- Metis Menu Js -->
-    <script src="../../../css/admin/js/jquery.metisMenu.js"></script>
+    <script src="../css/admin/js/jquery.metisMenu.js"></script>
       <!-- Custom Js -->
-    <script src="../../../css/admin/js/custom-scripts.js"></script>
+    <script src="../css/admin/js/custom-scripts.js"></script>
     
    
 </body>
