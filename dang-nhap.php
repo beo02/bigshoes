ceca353d2abcc416a203387fef853064bf27ddfd
beo02 <102813073+beo02@users.noreply.github.com>
2022-11-23 17:43:49 +0700

    Update dang-nhap.php

--- dang-nhap.php
+++ dang-nhap.php
@@ -35,7 +35,7 @@
         session_set_cookie_params($lifetime,'/');
         session_start();
         
-		require_once ('../admin/dao/khach-hang.php');
+		require_once ('../khach-hang.php');
 
 		extract($_REQUEST);
 		if(array_key_exists('btn_login',$_REQUEST)){
@@ -44,11 +44,11 @@
 				if($user['mat_khau'] == $mat_khau){
 					if($user['vai_tro'] == 1){
 						$_SESSION['user'] = $user;
-						header("location: ../trang-chinh/index.php");
+						header("location: ../index.php");
 					}
 					if($user['vai_tro'] == 0){
 						$_SESSION['user'] = $user;
-						header("location: ../trang-chinh/index.php");
+						header("location: ../index.php");
 					}
 				}
 			}else{
