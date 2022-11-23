54d4776ff9ba677ed809fbafab904f6b23631b07
beo02 <102813073+beo02@users.noreply.github.com>
2022-11-23 21:35:58 +0700

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
     function pdo_get_connection(){
-        $pdo = new PDO("mysql:host=SQL5030.site4now.net;dbname=db_a9046f_bigshoe",'a9046f_bigshoe','thanh1812');
+        $pdo = new PDO("mysql:host=mysql5030.site4now.net;dbname=db_a9046f_bigshoe",'a9046f_bigshoe','thanh1812');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;    
     }
